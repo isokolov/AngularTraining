4e04376d2398d4feb915307bf462ef0cb4ee6717
Illya Sokolov <illya.sokolov82@gmail.com>
2020-08-16 18:06:34 +0300

    ngStyle, ngClass, ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import {Post4Component} from './post4/post4.component';
 import {FormsModule} from '@angular/forms';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    Post4Component
+    Post4Component,
+    Post2Component
   ],
   imports: [
     BrowserModule, FormsModule
