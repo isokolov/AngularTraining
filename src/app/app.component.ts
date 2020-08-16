e1f33f2b6577e8dfb2394e44e97d5811c0980bf4
Illya Sokolov <illya.sokolov82@gmail.com>
2020-08-16 17:33:14 +0300

    Components, Interpolation, html to ts binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCourse2020';
+
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsm-dQHFmWp9Xw1e-4BfLDr67vBq5cil6OytRJExumqHUzTHVZ'
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over');
+      this.img = 'https://angular.io/assets/images/logos/angular/angular.png';
+    }, 5000);
+  }
 }
