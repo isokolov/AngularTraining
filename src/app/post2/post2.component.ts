3715b151fb5235da39ba3954643149b05462d78f
Illya Sokolov <illya.sokolov82@gmail.com>
2020-08-16 18:24:37 +0300

    ngSwitch, ngFor

--- src/app/post2/post2.component.ts
+++ src/app/post2/post2.component.ts
@@ -9,6 +9,22 @@ export class Post2Component implements OnInit {
 
   backgroundToggle = false;
   toggle = false;
+  toggle2: any;
+
+  arr = [1, 1, 2, 3, 5, 8, 13];
+
+  objs = [
+    {title: 'Post 1', author: 'Vladilen', comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'},
+      ]},
+    {title: 'Post 2', author: 'Vladilen 2', comments: [
+        {name: 'Max 2', text: 'lorem 1'},
+        {name: 'Max 2', text: 'lorem 2'},
+        {name: 'Max 2', text: 'lorem 3'},
+      ]}
+  ];
 
   constructor() { }
 
